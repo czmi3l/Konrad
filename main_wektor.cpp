30f422e0a766fa23b6086d085b0dd4b721b55acf
Konrad <konradam@o2.pl>
2016-02-26 17:50:59 +0100

    dla Maćka

---
+++ main_wektor.cpp
@@ -0,0 +1,15 @@
+#include "wektor.h"
+#include <vector>
+
+int main(){
+    wektor<int> b;
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    b.push_back(3);
+    cout<<b.Capacity()<<endl;
+}
