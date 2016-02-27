343d6b28edf84970b222410671801850e5c6d5f0
Konrad <konradam@o2.pl>
2016-02-27 20:50:12 +0100

    inne wartości w push_back

--- main_wektor.cpp
+++ main_wektor.cpp
@@ -8,7 +8,7 @@ int main(){
     b.push_back(3);
     b.push_back(3);
     b.push_back(3);
-    b.push_back(3);
-    b.push_back(3);
+    b.push_back(4);
+    b.push_back(5);
     cout<<b[2]<<endl;
 }
