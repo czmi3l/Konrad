f5362d2c5152c23161f052c199d87ff3eca28001
Konrad <konradam@o2.pl>
2016-02-27 21:03:55 +0100

    testujemy nowy branch

---
+++ klasy.cpp
@@ -0,0 +1,33 @@
+class poczta{
+public:
+    string nadawca, odbiorca, temat, tresc;
+    void wypisz();
+    void wczytaj();
+};
+
+void poczta::wypisz(){
+    cout<< nadawca<< endl;
+    cout<< odbiorca<< endl;
+    cout<< temat<< endl;
+    cout<< tresc<< endl;
+}
+
+void poczta::wczytaj(){
+    cin>>nadawca;
+    getline(cin,temat);
+}
+
+
+// funkcje poza klasa
+void wypisz(poczta Obiekt){
+    cout<< Obiekt.nadawca<< endl;
+    cout<< Obiekt.odbiorca<< endl;
+    cout<< Obiekt.temat<< endl;
+    cout<< Obiekt.tresc<< endl;
+}
+
+void wczytaj(poczta &Obiekt){
+    cin>>Obiekt.nadawca;
+    getline(cin, Obiekt.temat);
+}
+
